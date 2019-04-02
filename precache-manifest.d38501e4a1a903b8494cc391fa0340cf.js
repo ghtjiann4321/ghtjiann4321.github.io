c2f1b65e42caf266963a2ee573814aa99331aa12
Gian Torralba <giant@towa.network>
2019-04-02 18:24:49 +0800

    Initial

---
+++ precache-manifest.d38501e4a1a903b8494cc391fa0340cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1ff6d7565b8a862a2f99",
+    "url": "/static/js/main.5c1e41b4.chunk.js"
+  },
+  {
+    "revision": "4c4d455726bdd99a3e95",
+    "url": "/static/js/2.867bc3ee.chunk.js"
+  },
+  {
+    "revision": "1ff6d7565b8a862a2f99",
+    "url": "/static/css/main.b2255c84.chunk.css"
+  },
+  {
+    "revision": "5ba824779c36cc8432bbca4776d62a34",
+    "url": "/index.html"
+  }
+];
